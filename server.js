6619140a1a210abd1ea281689ce1ee7555ca9786
Lyderies <Lyderies@hotmail.com>
2020-12-08 11:04:57 +1300

    updated packages

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var express    = require("express");
-var mysql      = require('mysql');
+var mysql      = require('mysql2');
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'username',
